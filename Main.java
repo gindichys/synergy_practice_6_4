c0b0b485b95fb0fbba8244b480690a9438159d96
Gindich Yaroslav <gindichys@gmailcom>
2024-10-22 15:44:29 +0200

    read from file

---
+++ Main.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        String filePath = "example.txt";
+
+        List<String> lines = readFileLines(filePath);
+
+    }
+
+    private static List<String> readFileLines(String filePath) {
+        List<String> lines = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                lines.add(line);
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.err.println("Ошибка при чтении файла: " + e.getMessage());
+        }
+        return lines;
+    }
+}
