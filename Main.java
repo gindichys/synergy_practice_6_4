1c2e809146b2e1509561e6fd53f74f423567498f
Gindich Yaroslav <gindichys@gmailcom>
2024-10-22 16:42:57 +0200

    write to file

--- Main.java
+++ Main.java
@@ -1,25 +1,70 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String filePath = "example.txt";
+        Scanner scanner = new Scanner(System.in);
+        List<String> strings = new ArrayList<>();
 
-        List<String> lines = readFileLines(filePath);
+        System.out.println("Выберите вариант работы программы:");
+        System.out.println("1. Чтение из файла example.txt");
+        System.out.println("2. Ввод пользователем слов для сортировки и последующее сохранение в result.txt");
+        while (true) {
+            String input = scanner.nextLine();
+            switch (input) {
+                case "1": {
+                    String filePath = "example.txt";
 
-        List<String> sortedLines = sortLines(lines);
+                    List<String> lines = readFileLines(filePath);
 
-        System.out.println("Результат после сортировки:");
-        for (String line : sortedLines) {
-            System.out.println(line);
+                    List<String> sortedLines = sortLines(lines);
+
+                    System.out.println("Результат после сортировки:");
+                    for (String line : sortedLines) {
+                        System.out.println(line);
+                    }
+                    scanner.close();
+                    return;
+                }
+                case "2": {
+                    List<String> lines = inputLines();
+
+                    List<String> sortedLines = sortLines(lines);
+
+                    storeResultToFile(sortedLines);
+                    readFileLines("result.txt");
+
+                    scanner.close();
+                    return;
+                }
+                default: {
+                    System.out.println("Неверный ввод.");
+                }
+            }
+        }
+
+    }
+
+    private static void storeResultToFile(List<String> sortedLines) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("result.txt"))) {
+            for (String str : sortedLines) {
+                writer.write(str);
+                writer.newLine();
+            }
+            System.out.println("Результаты успешно сохранены в файл.");
+        } catch (IOException e) {
+            System.err.println("Ошибка при записи в файл: " + e.getMessage());
         }
     }
 
     private static List<String> readFileLines(String filePath) {
-        System.out.println("Прочитано из файла:");
+        System.out.println("Прочитано из файла " + filePath + ":");
         List<String> lines = new ArrayList<>();
 
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
@@ -38,5 +83,23 @@ public class Main {
         return list.stream()
                 .sorted(String::compareTo)
                 .toList();
+    }
+
+    private static List<String> inputLines() {
+        Scanner scanner = new Scanner(System.in);
+        List<String> strings = new ArrayList<>();
+
+        System.out.println("Введите строки для сортировки (введите 'exit' для завершения ввода):");
+
+        // Ввод строк от пользователя
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("exit")) { // Завершаем ввод, если пользователь ввёл 'exit'
+                break;
+            }
+            strings.add(input); // Добавляем введённую строку в список
+        }
+        scanner.close();
+        return strings;
     }
 }
